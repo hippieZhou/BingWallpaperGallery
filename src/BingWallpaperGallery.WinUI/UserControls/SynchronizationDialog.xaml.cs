0806be009d0b22fb01f01d085aafe2bb37da09e5
hippiezhou <hippiezhou@outlook.com>
2025-11-14 16:55:22 +0800

    💄 style: 完成 SettingsPage.xaml - 应用设置页

--- src/BingWallpaperGallery.WinUI/UserControls/SynchronizationDialog.xaml.cs
+++ src/BingWallpaperGallery.WinUI/UserControls/SynchronizationDialog.xaml.cs
@@ -1,6 +1,7 @@
 using BingWallpaperGallery.Core.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
@@ -9,6 +10,7 @@ using Microsoft.UI.Xaml.Controls;
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace BingWallpaperGallery.WinUI.UserControls;
+
 public sealed partial class SynchronizationDialog : ContentDialog
 {
     #region Singleton
@@ -31,12 +33,18 @@ public sealed partial class SynchronizationDialog : ContentDialog
     {
         args.Cancel = ViewModel.Cancel;
     }
+
+    private void OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+    {
+        ViewModel.LoadedCommand.Execute(null);
+    }
 }
 
 public partial class SynchronizationDialogViewModel : ObservableObject
 {
     private readonly DispatcherTimer _timer;
-    private readonly IGitHubStorageService _githubStorageService;
+    private readonly IMemoryCache _memoryCache;
+    private readonly IGitHubStorageService _githubStorage;
     private readonly ILogger<SynchronizationDialogViewModel> _logger;
 
     [ObservableProperty]
@@ -59,20 +67,34 @@ public partial class SynchronizationDialogViewModel : ObservableObject
         };
         _timer.Tick += (s, e) => Duration++;
 
-        _githubStorageService = App.GetService<IGitHubStorageService>();
+        _memoryCache = App.GetService<IMemoryCache>();
+        _githubStorage = App.GetService<IGitHubStorageService>();
         _logger = App.GetService<ILogger<SynchronizationDialogViewModel>>();
     }
 
     [RelayCommand(IncludeCancelCommand = true, AllowConcurrentExecutions = false)]
-    private async Task OnLoaded(CancellationToken cancellationToken = default)
+    private Task OnLoaded(CancellationToken cancellationToken = default)
     {
+        Message = "该操作是全量同步，可能会消耗较长时间，请耐心等待...";
+        IsLoading = false;
+        Duration = 0;
+        return Task.CompletedTask;
+    }
+
+    [RelayCommand(IncludeCancelCommand = true, AllowConcurrentExecutions = false)]
+    private async Task OnSync(CancellationToken cancellationToken = default)
+    {
+        Cancel = true;
+
         if (!_timer.IsEnabled)
         {
             Duration = 0;
             _timer.Start();
         }
 
-        await _githubStorageService.RunAsync(
+        IsLoading = true;
+
+        await _githubStorage.RunAsync(
             onLoading: msg =>
             {
                 Message = msg;
@@ -82,23 +104,34 @@ public partial class SynchronizationDialogViewModel : ObservableObject
             {
                 Message = "同步完成！您可以关闭此对话框。";
                 IsLoading = false;
+                _timer.Stop();
+                if (_memoryCache is MemoryCache memoryCache)
+                {
+                    memoryCache.Clear();
+                }
             },
             onError: ex =>
             {
                 _logger.LogError("获取 GitHub 归档文件失败: {message}", ex.Message);
                 Message = "获取 GitHub 归档文件失败，请检查网络连接或稍后重试。";
+                IsLoading = false;
+                _timer.Stop();
+                if (_memoryCache is MemoryCache memoryCache)
+                {
+                    memoryCache.Clear();
+                }
             }, cancellationToken);
     }
 
     [RelayCommand]
-    private void OnCloseButton()
+    private void OnClose()
     {
         if (_timer.IsEnabled)
         {
             _timer.Stop();
         }
 
-        loadedCommand.Cancel();
+        SyncCommand.Cancel();
         Cancel = false;
     }
 }
